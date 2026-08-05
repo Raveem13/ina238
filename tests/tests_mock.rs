665c9e4cc2684a98b5c149a4dc473d071e47b189
Raveem KG <gkraveem@gmail.com>
2026-08-05 13:33:29 +0530

    test: Add more tests
    Tests added for:
    - Temperature, Current, Power
    - Shunt overvolt & undervolt, Bus overvolt & undervolt

--- tests/tests_mock.rs
+++ tests/tests_mock.rs
@@ -91,23 +91,95 @@ mod tests {
         i2c.done();
     }
 
+    // Shunt voltage test
     #[test]
     fn test_shunt_volt() {
-        // let exp_val = exp_shunt_volt(2.0, true);
         let mut i2c = I2cMock::new(&[read_txn(0x04, &0x0020_u16.to_be_bytes())]);
         let mut ina = INA238::new(i2c.clone(), DEFAULT_ADDRESS);
         ina.shunt_voltage().unwrap();
         i2c.done();
     }
 
+    // Bus voltage test
     #[test]
     fn test_bus_volt() {
-        // let exp_val = exp_shunt_volt(2.0, true);
         let mut i2c = I2cMock::new(&[read_txn(0x05, &0x0010_u16.to_be_bytes())]);
         let mut ina = INA238::new(i2c.clone(), DEFAULT_ADDRESS);
         ina.bus_voltage().unwrap();
         i2c.done();
     }
+
+    // Internal die temperature test
+    #[test]
+    fn test_die_temp() {
+        let mut i2c = I2cMock::new(&[read_txn(0x06, &0x0040_u16.to_be_bytes())]);
+        let mut ina = INA238::new(i2c.clone(), DEFAULT_ADDRESS);
+        ina.die_temperature().unwrap();
+        i2c.done();
+    }
+
+    // Current result test
+    #[test]
+    fn test_current() {
+        let mut i2c = I2cMock::new(&[read_txn(0x07, &0x0001_u16.to_be_bytes())]);
+        let mut ina = INA238::new(i2c.clone(), DEFAULT_ADDRESS);
+        ina.current().unwrap();
+        i2c.done();
+    }
+
+    // Power result test
+    #[test]
+    fn test_power() {
+        let mut i2c = I2cMock::new(&[read_txn(0x08, &u24_be_bytes(0x0020))]);
+        let mut ina = INA238::new(i2c.clone(), DEFAULT_ADDRESS);
+        ina.power().unwrap();
+        i2c.done();
+    }
+
+    // Test shunt overvoltage threshold
+    #[test]
+    fn test_shunt_overvolt() {
+        // 0x7FFF (32727) * 1.25uv = 0.04 V
+        let exp_val = exp_shunt_volt(0.04, true) as u16;
+        let mut i2c = I2cMock::new(&[write_txn(0x0C, exp_val)]);
+        let mut ina = INA238::new(i2c.clone(), DEFAULT_ADDRESS);
+        ina.set_shunt_overvoltage_th(0.04).unwrap();
+        i2c.done();
+    }
+
+    // Test shunt undervoltage threshold
+    #[test]
+    fn test_shunt_undervolt() {
+        // 0x8000 (-32728) * 1.25uv = -0.04 V
+        let exp_val = exp_shunt_volt(-0.04, true) as u16;
+        let mut i2c = I2cMock::new(&[write_txn(0x0D, exp_val)]);
+        let mut ina = INA238::new(i2c.clone(), DEFAULT_ADDRESS);
+        ina.set_shunt_undervoltage_th(-0.04).unwrap();
+        i2c.done();
+    }
+
+    // Test Bus overvoltage threshold
+    #[test]
+    fn test_bus_overvolt() {
+        // 0x7FFF (32727) * 3.125mv = 102.2 V
+        let exp_val = (102.0 / 3.125e-3) as u16;
+        let mut i2c = I2cMock::new(&[write_txn(0x0E, exp_val)]);
+        let mut ina = INA238::new(i2c.clone(), DEFAULT_ADDRESS);
+        ina.set_bus_overvoltage_th(102.0).unwrap();
+        i2c.done();
+    }
+
+    // Test bus undervoltage threshold
+    #[test]
+    fn test_bus_undervolt() {
+        // 0x1 * 3.125mv = 0.003 V
+        let exp_val = (0.003 / 3.125e-3) as u16;
+        let mut i2c = I2cMock::new(&[write_txn(0x0F, exp_val)]);
+        let mut ina = INA238::new(i2c.clone(), DEFAULT_ADDRESS);
+        ina.set_bus_undervoltage_th(0.003).unwrap();
+        i2c.done();
+    }
+
     // ---- Helper functions ----
     fn read_txn(reg: u8, bytes: &[u8]) -> I2cTransaction {
         I2cTransaction::write_read(DEFAULT_ADDRESS as u8, vec![reg], bytes.to_vec())
@@ -116,6 +188,10 @@ mod tests {
     fn write_txn(reg: u8, value: u16) -> I2cTransaction {
         let bytes = value.to_be_bytes();
         I2cTransaction::write(DEFAULT_ADDRESS as u8, vec![reg, bytes[0], bytes[1]])
+    }
+
+    fn u24_be_bytes(value: u32) -> [u8; 3] {
+        [(value >> 16) as u8, (value >> 8) as u8, value as u8]
     }
 
     //Expected shunt constant (SHUNT_CAL) calculation
@@ -150,5 +226,15 @@ mod tests {
             | (shuntvolt_ct as u16) << 6
             | (tempe_ct as u16) << 3
             | adc_avgcount as u16
+    }
+
+    // Shunt voltage calculations
+    fn exp_shunt_volt(volt: f32, adc_range: bool) -> i16 {
+        let mut shunt_volt = volt / 5e-6;
+        if adc_range {
+            shunt_volt *= 4.0;
+        }
+        println!("The value is: {:#x}", shunt_volt as u16);
+        shunt_volt as i16
     }
 }
